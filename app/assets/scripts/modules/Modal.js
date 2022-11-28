a9705b56f40cf7cde9211a41f61a021557910a36
iamverg <dev01.dash@gmail.com>
2022-11-28 20:39:18 +0100

    implemented webpack dynamic import code splitting for modal

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -4,21 +4,17 @@ export default class Modal {
     this.injectHTML();
     this.modal = document.querySelector(".modal");
     this.closeIcon = document.querySelector(".modal__close");
-    this.openModalButtons = document.querySelectorAll(".open-modal");
     this.events();
   }
 
   events() {
-    // listen for open click
-    this.openModalButtons.forEach((el) => el.addEventListener('click', (e) => this.openTheModal(e)));
     // listen for close click
     this.closeIcon.addEventListener('click', () => this.closeTheModal())
     // pushes any key
     document.addEventListener('keyup', e => this.keyPressHandler(e));
   }
 
-  openTheModal(e) {
-    e.preventDefault();
+  openTheModal() {
     this.modal.classList.add("modal--is-visible");
   }
 
