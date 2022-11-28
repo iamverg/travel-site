8af2171066c2b602737bcf140c5c3f0be3d3d927
iamverg <dev01.dash@gmail.com>
2022-11-28 18:21:03 +0100

    added modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 if (module.hot) {
   module.hot.accept();
@@ -9,5 +10,6 @@ if (module.hot) {
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
+new Modal();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
